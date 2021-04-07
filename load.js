4e1b4d3984acfe256d3014967020b26904cbc47a
jektiss <78919455+jektiss@users.noreply.github.com>
2021-04-07 21:07:14 +0200

    go

--- load.js
+++ load.js
@@ -12,12 +12,10 @@ function loadRankings () {
             console.warn(e);
         }
     };
-
     request.send();
 }
 
 function populateRankings (json, rankingsBody) {
-    // Populate Leaderboard
     let count = 1;
     for(let user of json) {
         let tr = document.createElement("tr");
@@ -33,6 +31,22 @@ function populateRankings (json, rankingsBody) {
         rankingsBody.appendChild(tr);
         count++;
     }
+}
+
+function info(){
+    alert("How does the leaderboard work?\n" +
+        "\n" +
+        "You will get 1 Point for 1$ generetad Netrake.\n" +
+        "\n" +
+        "Netrake = Rake (after deducting PVI) – Fish Buffet/Black Status (Rakeback) – Welcome Bonus – Transaction fees – Tournament Overlay \n" +
+        "\n" +
+        "Promotions like daily leaderboards or Jackpots have no influence on the Netrake!\n" +
+        "\n" +
+        "The leaderboard prize money can vary and is dependent on the previous month's generated Netrake from all players.\n" +
+        "\n" +
+        "The leaderboard will be updated every Monday.\n" +
+        "\n" +
+        "Negative Netrake will carry over to the following month and be considered/deducted for next month's payout.");
 }
 
 document.addEventListener("DOMContentLoaded", () => { loadRankings (); });
